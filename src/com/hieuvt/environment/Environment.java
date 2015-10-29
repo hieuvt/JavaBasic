e3e2b401c737417e9cf57eff40f03bd267f4ec8b
hieuvt <hieuvt2008@gmail.com>
2015-10-29 15:40:41 +0700

    Add environment check

---
+++ src/com/hieuvt/environment/Environment.java
@@ -0,0 +1,10 @@
+package com.hieuvt.environment;
+
+public class Environment {
+
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperties());
+        System.out.println(System.getProperty("user.timezone"));
+    }
+}
